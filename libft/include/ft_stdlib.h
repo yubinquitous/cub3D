4f9ff38930eee25b6eb27afbfb58641dd3a8491b
yubchoi <yubchoi@student.42seoul.kr>
2022-12-28 16:58:32 +0900

    [CHORE] norm

--- libft/include/ft_stdlib.h
+++ libft/include/ft_stdlib.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_stdlib.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yubin <yubchoi@student.42>                 +#+  +:+       +#+        */
+/*   By: yubchoi <yubchoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 09:29:46 by kijsong           #+#    #+#             */
-/*   Updated: 2022/12/26 20:44:15 by yubin            ###   ########.fr       */
+/*   Updated: 2022/12/28 16:56:52 by yubchoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,5 @@ char	*ft_itoa(int value, char *buffer, int radix);
 void	*safe_malloc(size_t size);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_free(void **ptr);
-
-void	*ft_bsearch(const void *key, const void *base, size_t nmemb, size_t size);
 
 #endif
--- libft/src/stdlib/safe_malloc.c
+++ libft/src/stdlib/safe_malloc.c
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   safe_malloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yubchoi <yubchoi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/28 16:57:25 by yubchoi           #+#    #+#             */
+/*   Updated: 2022/12/28 16:57:40 by yubchoi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-void *safe_malloc(size_t size)
+void	*safe_malloc(size_t size)
 {
-    void    *ptr;
+	void	*ptr;
 
-    ptr = malloc(size);
-    if (!ptr)
-    {
-        perror("Error\ncub3D: ");
-        exit(1);
-    }
-    return (ptr);
+	ptr = malloc(size);
+	if (!ptr)
+	{
+		perror("Error\ncub3D: ");
+		exit(1);
+	}
+	return (ptr);
 }
